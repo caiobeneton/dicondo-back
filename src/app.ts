7c7bbdb637dec1759d3ce367f33c6c178bf3333e
Caio Beneton <caio.beneton@gmail.com>
2023-04-04 20:15:17 -0300

    chore: cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { json } from "express";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 app.get("/health", (req: Request, res: Response) => {
   res.send("Ok");
